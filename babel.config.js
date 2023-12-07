abd6c4dd0a7c03ddef7c1bb3854785e1f6ad0148
Ruan Carlos <ruancarlosmeira@gmail.com>
2023-12-07 18:15:19 -0300

    Refactor file paths and dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@components': './src/components',
+          '@config': './src/config',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+          '@types': './src/types',
+          '@schemas': './src/schemas',
+        },
+      },
+    ],
+  ],
 };
