be0daf669533d7393dba4a3b069435d908e49f0c
Ruan Carlos <ruancarlosmeira@gmail.com>
2023-12-08 16:00:43 -0300

    Refactor CarDetails screen and add new features

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Wrapper, PicturesSlider, Slider, SliderImage} from './styles';
+import React, {useState} from 'react';
+
+// styles
+import {
+  Wrapper,
+  PicturesSlider,
+  Slider,
+  SliderImage,
+  SliderImageWrapper,
+  CarName,
+  ActionArea,
+  Content,
+  Row,
+  IconMap,
+  CarCity,
+  ScrollView,
+  CarInfoWrapper,
+  CarInfoCard,
+  CarInfoTitle,
+  CarInfo,
+} from './styles';
+import {useTheme} from 'styled-components/native';
+
+// models
 import {CarProps} from '@models/Cars';
 
+// components
+import {PrimaryButton} from '@components/Button';
+import {SliderIndicator} from './SlideIndicator';
+
 const renderImage = ({item}: any) => {
-  console.log(item);
-  return <SliderImage source={{uri: item.image}} />;
+  return (
+    <SliderImageWrapper>
+      <SliderImage source={{uri: item.image}} />
+    </SliderImageWrapper>
+  );
 };
 
 export default function CarDetails({route}: {route: any}) {
   const {car} = route.params as {car: CarProps};
+  const {dimensions} = useTheme();
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const getSliderImages = () => {
     return car.photos.map((photo, index) => ({
@@ -20,10 +50,74 @@ export default function CarDetails({route}: {route: any}) {
 
   return (
     <Wrapper>
-      <PicturesSlider>
-        <Slider data={getSliderImages()} renderItem={renderImage} />
-      </PicturesSlider>
-      <Text>CarDetails</Text>
+      <ScrollView>
+        <PicturesSlider>
+          <Slider
+            data={getSliderImages()}
+            renderItem={renderImage}
+            keyExtractor={(item) => {
+              const {key} = item as {key: number};
+              return key.toString();
+            }}
+            snapToAlignment="start"
+            decelerationRate="fast"
+            snapToInterval={dimensions.width}
+            onScroll={(event) => {
+              const {contentOffset} = event.nativeEvent;
+              setCurrentSlide(Math.round(contentOffset.x / dimensions.width));
+            }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          />
+          <SliderIndicator current={currentSlide} total={car.photos.length} />
+        </PicturesSlider>
+        <Content>
+          <CarName>{car.name}</CarName>
+          <Row>
+            <IconMap name="map-pin" />
+            <CarCity>{car.city}</CarCity>
+          </Row>
+
+          <CarInfoWrapper>
+            <CarInfoCard>
+              <CarInfoTitle>Marca</CarInfoTitle>
+              <CarInfo>{car.brand}</CarInfo>
+            </CarInfoCard>
+
+            <CarInfoCard>
+              <CarInfoTitle>Ano</CarInfoTitle>
+              <CarInfo>{car.year}</CarInfo>
+            </CarInfoCard>
+
+            <CarInfoCard>
+              <CarInfoTitle>Modelo</CarInfoTitle>
+              <CarInfo>{car.model}</CarInfo>
+            </CarInfoCard>
+
+            <CarInfoCard>
+              <CarInfoTitle>Km's</CarInfoTitle>
+              <CarInfo>{car.mileage || 'N/A'}</CarInfo>
+            </CarInfoCard>
+
+            <CarInfoCard>
+              <CarInfoTitle>Motor</CarInfoTitle>
+              <CarInfo>{car.gear || 'N/A'}</CarInfo>
+            </CarInfoCard>
+
+            <CarInfoCard>
+              <CarInfoTitle>Combústivel</CarInfoTitle>
+              <CarInfo>{car.fuel || 'N/A'}</CarInfo>
+            </CarInfoCard>
+          </CarInfoWrapper>
+        </Content>
+      </ScrollView>
+
+      <ActionArea>
+        <PrimaryButton
+          title="Entrar em contato"
+          onPress={() => console.log('alugou')}
+        />
+      </ActionArea>
     </Wrapper>
   );
 }
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,26 +1,112 @@
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-export const Wrapper = styled.ScrollView`
+export const Wrapper = styled.View`
   flex: 1;
-  background-color: ${({theme}) => theme.colors.background_primary};
+`;
+
+export const ScrollView = styled.ScrollView`
+  flex: 1;
+  margin-bottom: ${({theme}) => theme.spacing.px[15]};
 `;
 
 export const PicturesSlider = styled.View`
   height: 250px;
   width: 100%;
+
+  border-bottom-width: ${({theme}) => theme.spacing.px[1]};
+  border-bottom-style: solid;
+  border-bottom-color: ${({theme}) => theme.colors.primary[500]};
 `;
 
-export const Slider = styled.FlatList.attrs({
-  showsHorizontalScrollIndicator: false,
-  horizontal: true,
-})`
-  height: 250px;
+export const Slider = styled.FlatList`
+  height: 100%;
   width: 100%;
-  background-color: red;
+`;
+
+export const SliderImageWrapper = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  width: ${({theme}) => theme.dimensions.width}px;
 `;
 
 export const SliderImage = styled.Image`
-  height: 250px;
   width: 100%;
+  height: 250px;
+
   resize-mode: cover;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  padding: ${({theme}) => theme.spacing.px[4]};
+`;
+
+export const ActionArea = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  flex: 1;
+  padding: ${({theme}) => theme.spacing.px[4]};
+  position: absolute;
+  bottom: 0;
+`;
+
+export const CarName = styled.Text`
+  font-family: ${({theme}) => theme.font.family.primary};
+  font-size: ${({theme}) => theme.font.size[7]};
+  color: ${({theme}) => theme.colors.base[900]};
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding-top: ${({theme}) => theme.spacing.px[1]};
+  padding-bottom: ${({theme}) => theme.spacing.px[1]};
+`;
+
+export const IconMap = styled(Icon).attrs({
+  size: 20,
+})`
+  margin-right: ${({theme}) => theme.spacing.px[1]};
+  color: ${({theme}) => theme.colors.base[900]};
+`;
+
+export const CarCity = styled.Text`
+  font-family: ${({theme}) => theme.font.family.primary};
+  font-size: ${({theme}) => theme.font.size[2]};
+  color: ${({theme}) => theme.colors.base[600]};
+`;
+
+export const CarInfoWrapper = styled.View`
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  padding-top: ${({theme}) => theme.spacing.px[4]};
+  padding-bottom: ${({theme}) => theme.spacing.px[4]};
+  gap: ${({theme}) => theme.spacing.px[2]};
+`;
+
+export const CarInfoCard = styled.View`
+  background-color: ${({theme}) => theme.colors.base[300]};
+  padding: ${({theme}) => theme.spacing.px[4]};
+  border-radius: ${({theme}) => theme.spacing.px[2]};
+
+  flex-direction: column;
+  width: 120px;
+  justify-content: space-between;
+`;
+
+export const CarInfoTitle = styled.Text`
+  font-family: ${({theme}) => theme.font.family.primary};
+  font-size: ${({theme}) => theme.font.size[3]};
+  color: ${({theme}) => theme.colors.base[600]};
+`;
+
+export const CarInfo = styled.Text`
+  font-family: ${({theme}) => theme.font.family.primary};
+  font-size: ${({theme}) => theme.font.size[2]};
+  color: ${({theme}) => theme.colors.base[900]};
 `;
